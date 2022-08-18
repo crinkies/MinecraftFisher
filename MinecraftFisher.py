ebe062a885c3e94150903baf0fed1cae474f5e9e
☆ crinkies ☆ <101161611+crinkies@users.noreply.github.com>
2022-08-18 02:31:50 -0400

    Update MinecraftFisher.py

--- MinecraftFisher.py
+++ MinecraftFisher.py
@@ -53,7 +53,7 @@ def fisher():
         if quitFishing:
             break
         try:
-            x, y = pyautogui.locateCenterOnScreen('images\\bobber.png', region=(w2,h2,w1,h1), confidence=0.8)
+            x, y = pyautogui.locateCenterOnScreen('images\\bobber1.png', region=(w2,h2,w1,h1), confidence=0.8)
             click(x,y)
             time.sleep(3)
         except:
